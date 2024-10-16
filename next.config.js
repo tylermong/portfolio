51866be22f03a7fda88b5910a654ce9b4463612d
Tyler Mong <117101211+tylermong@users.noreply.github.com>
2024-10-16 10:38:32 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,12 @@
 /**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
+ * This is especially useful for Docker builds.
  */
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
 const config = {
-  // Enable static export mode
-  output: 'export',
-
-  // You can add more Next.js configuration options here if needed
+  output: 'export', // This enables static export
 };
 
 export default config;
